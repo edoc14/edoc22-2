b80762afd7112fc56abcbde933b1c47cf4aee617
euWna <112548635+euWna@users.noreply.github.com>
2022-10-08 00:54:24 +0900

    초기 커밋.

---
+++ 9466_1976239.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int T, n;
+
+typedef struct TreeNode {
+	int num;
+	TreeNode* select;
+};
+
+int main() {
+	cin >> T;
+	while (T--) {
+		cin >> n;
+		
+
+
+	}
+
+
+	return 0;
+}
