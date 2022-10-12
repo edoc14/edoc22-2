b5cb2c02dac162def7ee6ee3eb857774bd5c728d
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2022-10-12 22:25:05 +0900

    Add files via upload

--- 10월05일/초중급/2071087_13565.cpp
+++ 10월05일/초중급/2071087_13565.cpp
@@ -1,49 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <algorithm>
-#include <vector>
-
 using namespace std;
 
-void dfs(int start, vector<int> graph[], bool check[]){
-    check[start]=true;
+int N, M;
+const int MAX = 1001;
+int map[MAX][MAX];
+bool visited[MAX][MAX];
+int dy[] = { 0,0,-1,1 };
+int dx[] = { -1,1,0,0 };
 
-    for (int i = 0; i < graph[start]; i++) {
-        int next = v[start][i];
+void DFS(int  y, int x) { //깊이 우선 탐색
+    visited[y][x] = true;
 
-        if (!check[next]){
-            dfs(next, graph, check);
+    for (int i = 0; i < 4; i++) { //방향 확인
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || nx < 0 || ny >= M || nx >= N)
+            continue;
+        if (map[ny][nx]==0 && !visited[ny][nx]) {
+            visited[ny][nx] = true; //방문 확인하면 true
+            DFS(ny, nx);
         }
     }
 }
 
 int main() {
-    int m,n;
-    cin >> m>> n;
-
-    vector<int>graph[n+1];
-
-    for (int i = 0; i <m; i++) {
-        int u,v;
-        for (int j = 0; j < n; j++) {
-            cin>>
+    cin >> M >> N;
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%1d", &map[i][j]);
         }
-        cin>>u>>v;
-
-        graph[u].push_back(v);
-        graph[v].push_back(u);
     }
-    dfs();
-    return 0;
 
-    /*
-     * 2차원 배열 입력 받고
-     * x[0][j] j 1~n까지 반복
-     * 1인 경우 i++
-     * 예를 들어 x[0][1] = 1이다.
-     * x[1][1] 이 1인지 확인 -> 1이면 양옆 1인지 확인
-     * x[1][1] 이 0이면 x[0][2], x[0][3] 반복문 계속
-     * x[1][1] 1이면 x[1][j-1]과 x[1][j+1]이 1인지 확인 -> 이과정 반복
-     * x[m][j] 도달 후, 1이 하나라도 있으면, 반복문 종료 후 "YES" 출력
-     * x[m][j] 도달 후, 0 뿐이면 "NO" 출력
-     */
+    for (int j = 0; j < N; j++) {
+        if (map[0][j]==0 && !visited[0][j]) {
+            DFS(0, j);
+        }
+    }
+
+    bool flag = false;
+    for (int j = 0; j < N; j++) {
+        if (visited[M-1][j]) {
+            flag = true;
+        }
+    }
+
+    if (flag == true)
+        cout << "YES";
+    else
+        cout << "NO";
 }
