32c2a3f8f29b830b426d061d6d627cf9f714e920
R1NM <shinzeya@gmail.com>
2022-11-02 19:49:06 +0900

    no message

---
+++ 11월02일/초중급/1976211_2477.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int len[5][2];
+int K;
+int shape[5];
+int ans;
+int main() {
+    cin>>K;
+    int w,l;
+    for (int i = 0; i < 6; ++i) {
+        cout<<0;
+    }
+
+    return 0;
+}
