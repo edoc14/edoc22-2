14695b391b4785331c559389dfce0dc00a864b0e
Ssomewhat <isc10120@gmail.com>
2022-11-02 18:58:46 +0900

    Create BOJ.c

---
+++ 11월02일/초중급/BOJ.c
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+
+int main() {
+	int a, b, c, res;
+	scanf("%d%d%d", &a, &b, &c);
+	if (b >= c) res = -1;
+	else  res = a / (c - b) + 1;
+	printf("%d", res);
+	return 0; //전희원
+}
