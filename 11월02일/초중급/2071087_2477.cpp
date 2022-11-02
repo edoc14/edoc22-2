3ebf5208aeda591279d25fdae6219ff8602dece4
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2022-11-02 19:35:45 +0900

    Add files via upload

---
+++ 11월02일/초중급/2071087_2477.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int K,x,y,typ, txp, area=0;
+    cin>>K;
+    for(int i=0;i<6;i++){
+        cin>>x>>y;
+        
+    }
+    cout<<area*K;
+}
