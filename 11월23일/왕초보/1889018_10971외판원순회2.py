c944901d0cde6be373918af67e326f95a9f5b674
sangmin <1889018@ewhain.net>
2022-11-29 15:40:19 +0900

    1123 실패 수정

--- 11월23일/왕초보/1889018_10971외판원순회2.py
+++ 11월23일/왕초보/1889018_10971외판원순회2.py
@@ -1,3 +1,5 @@
+# https://jjangsungwon.tistory.com/4
+
 import sys
 input = sys.stdin.readline
 
@@ -6,13 +8,24 @@ cost = []
 
 for n in range(N):
     cost.append(list(map(int, input().split(' '))))
-
-for n in range(N):
-    visited = [False for n in range(N)]
-    visited[n]=True
     
-def recur(N, visited, cost):
-    for n in range(N):
-        if not visited[n]:
-             visited[n] = True
-             
+def dfs(start, next, value, visited):
+    global min_value
+
+    if len(visited) == N:
+        if cost[next][start] != 0:
+            min_value = min(min_value, value + cost[next][start])
+        return
+
+    for i in range(N):
+        if cost[next][i] != 0 and i != start and i not in visited:
+            visited.append(i)
+            dfs(start, i, value + cost[next][i], visited)
+            visited.pop()
+            
+min_value = sys.maxsize
+
+for i in range(N):
+    dfs(i, i, 0, [i])
+
+print(min_value)
